470b2d226f5b51133d82d4d30bc21690ffea149c
M Sathya Kumar <56182934+Sathyakumar-M@users.noreply.github.com>
2023-01-22 19:41:30 +0530

    Java Practice Projects

---
+++ Java_Practice_Projects/src/com/sathya/practice/EvenOdd.java
@@ -0,0 +1,9 @@
+package com.sathya.practice;
+public class EvenOdd {
+	
+	public static void main(String[] args) {
+		int a = 0;
+		System.out.println(a%2==0 ?"EVEN":"ODD");
+	}
+
+}
---
+++ Java_Practice_Projects/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SATHYA KUMAR M
+ *
+ */
+module Java_Practice_Projects {
+}
